96691e55399da6525145fdf2490e98ab6f267217
Ariel Messing <ariel.messing@gmail.com>
2026-04-13 20:36:33 +0100

    Filter draft notes beginning with underscore

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { glob } from "astro/loaders";
 import { z } from "astro/zod"
 
 const notes = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/notes" }),
+  loader: glob({ pattern: "**/[!_]*.{md,mdx}", base: "./src/content/notes" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
