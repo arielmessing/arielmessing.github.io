41af51ebb6b2869f5eb60f0cb64f5d5d90dac22e
Ariel Messing <ariel.messing@gmail.com>
2026-06-14 12:23:19 +0100

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://arielmessing.github.io",
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: "github-light",
